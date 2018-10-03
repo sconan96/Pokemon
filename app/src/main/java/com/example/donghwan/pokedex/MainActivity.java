f5dee6be5c849e814171371e472421b1816b7c42
sconan96 <wnrdjsj5755@naver.com>
2018-10-03 16:05:50 +0900

    Add One Page

--- app/src/main/java/com/example/donghwan/pokedex/MainActivity.java
+++ app/src/main/java/com/example/donghwan/pokedex/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.donghwan.pokedex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton tobulbasuar = (ImageButton) findViewById(R.id.Bulbasaur001);
+        tobulbasuar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Main1Activity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
 }
