99edb68c3e8ee550271905e82d472098a461d219
sconan96 <wnrdjsj5755@naver.com>
2019-01-23 23:40:16 +0900

    Add and fix

---
+++ app/src/main/java/com/example/donghwan/pokedex/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.donghwan.pokedex;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread myThread = new Thread(){
+
+            public void run() {
+                try {
+                    sleep(1250);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
